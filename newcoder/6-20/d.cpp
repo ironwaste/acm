b42a35007c49474f028eb1f9556ddb5e80d0594d
freshsun-y9000p <ironwaste97213@outlook.com>
2025-06-26 19:42:21 +0800

    牛客练习赛141 字典树+双dfs搜索以及dfs逆序取全

--- newcoder/6-20/d.cpp
+++ newcoder/6-20/d.cpp
@@ -1,40 +1,98 @@
-// 看什么看,以为我厉害吗？
-// 我只是戾气很重,不厉害只需要114514分钟就能变厉害了
-// 而你我的朋友只需要2.5年就可以了
 #include<bits/stdc++.h>
-#include<climits>
-
-
 #define all(x) x.begin(),x.end()
-#define pb(x) push_back(x)
-#define i64 long long
-#define fi first
-#define se second
+using i64 = long long;
 
-#define endl '\n'
-#define mod3 998244353
-#define mod7 1000000007
-#define de(x) cerr << #x <<" "<<x <<" ";
-#define deb(x) cerr << #x <<" "<<x <<endl;
-using namespace std;
+struct Trie {
+    struct node {
+        std::array<int, 2> son;
 
-using pii = pair<int,int>;
-using pll = pair<i64, i64>;
+        node() : son{} {}
+    };
 
-/*
-树上异或 ： 最大值
-*/
-void solve() {
+    std::vector<node> t;
 
+    Trie() {
+        init();
+    }
+
+    void init() {
+        t.assign(1, node());
+    }
+
+    int newnode() {
+        t.emplace_back();
+        return t.size() - 1;
+    }
+
+    void insert(const int &val) {
+        int p = 0;
+        for (int i = 30; i >= 0; i--) {
+            int ne = val >> i & 1;
+            if (t[p].son[ne] == 0) {
+                t[p].son[ne] = newnode();
+            }
+            p = t[p].son[ne];
+        }
+    }
+
+    int query(const int &val) {
+        int p = 0, ans = 0;
+        for (int i = 30; i >= 0; i--) {
+            int ne = val >> i & 1;
+            if (t[p].son[ne ^ 1]) {
+                ans |= 1 << i;
+                p = t[p].son[ne ^ 1];
+            } else {
+                p = t[p].son[ne];
+            }
+        }
+        return ans;
+    }
+};
+
+void DAOQI() {
+    int n;
+    std::cin >> n;
+    std::vector<std::vector<int>> adj(n + 1);
+    std::vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        std::cin >> a[i];
+    }
+    for (int i = 1; i < n; i++) {
+        int u, v;
+        std::cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    Trie trie;
+    std::vector<int> ans(n + 1);
+    auto dfs = [&](auto &&dfs, int u, int fa) -> void {
+        ans[u] = std::max(ans[u], trie.query(a[u]));
+        for (int v: adj[u]) {
+            if (v == fa) continue;
+            dfs(dfs, v, u);
+        }
+        trie.insert(a[u]);
+    };
+    dfs(dfs, 1, 1);
+
+    for (int i = 1; i <= n; i++) {
+        reverse(all(adj[i]));
+    }
+    trie.init();
+    dfs(dfs, 1, 1);
+    for (int i = 1; i <= n; i++) {
+        std::cout << ans[i] << " \n"[i == n];
+    }
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
     int T = 1;
-    while(T--){
-        solve();
-    }
+    //std::cin >> T;
+    while (T--) DAOQI();
     return 0;
 }
+
--- newcoder/6-20/d_make.cpp
+++ newcoder/6-20/d_make.cpp
@@ -23,31 +23,86 @@ using pll = pair<i64, i64>;
 
 const int SIZE = 60;
 const int N = 1e6;
-struct TireNode {
-    char ch;
-    int isEnd, num;
-    // num 是前缀的出现次数
-    TireNode* son[SIZE];
-    TireNode():{};
-    TireNode(int x,int a):isEnd(x),num(a) {};
-}t[N];
-int cnt = 1; // cnt = 0 is root node
-void insert(string s) {
-    s[]
-    int now = 0;
-    for (int i = 0;;i ++) {
-        
+
+struct Trie {
+    struct node {
+        array<int, 2>son;
+        node() :son{} {}
+    };
+    vector<node>t;
+
+    Trie() { init(); }
+    void init() { t.assign(1, node()); }
+    // 初始化 vector，设置大小为1并赋值为 node()
+    i64 newnode() {
+        t.emplace_back();
+        return t.size() - 1;
     }
 
-}
+
+    void insert(i64 x) {
+        int p = 0;
+        // 1 <= x <= 1e9
+        for (int i = 30;~i;i--) {
+            int v = x >> i & 1;
+            if (t[p].son[v] == 0) {
+                t[p].son[v] = newnode();
+            }
+            p = t[p].son[v];
+        }
+    }
+
+    i64 query(i64 x) {
+        i64 p = 0, ans = 0;
+        for (int i = 30;~i;i--) {
+            i64 v = x >> i & 1;
+            if (t[p].son[v ^ 1]) {
+                ans |= 1 << i;
+                p = t[p].son[v ^ 1];
+            } else {
+                p = t[p].son[v];
+            }
+        }
+        return ans;
+    }
+};
+
 
 void solve(){
-    int n, q;
-    cin >> n >> q;
+    int n;
+    cin >> n;
+    vector<i64>a(n),ans(n,0);
+    for (int i = 0;i < n;i++) { cin >> a[i]; }
 
-
-
-
+    vector<vector<i64> >g(n);
+    for (int i = 0;i < n - 1;i ++) {
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    // printf(n);
+    // deb(n);
+    Trie trie;
+    auto dfs = [&](auto self,int fa, int u)->void {
+        ans[u] = max(ans[u], trie.query(a[u]));// 关键在这里，没想清楚 
+        for (auto v : g[u]) {
+            if (fa == v)continue;
+            self(self, u, v);
+        }
+        trie.insert(a[u]);
+        };
+    dfs(dfs, 0, 0);
+    // deb(n);
+    for (int i = 0;i < n;i ++) {
+        reverse(all(g[i]));
+    }
+    trie.init();
+    dfs(dfs, 0, 0);
+    for (int i = 0;i < n;i ++) {
+        cout << ans[i] << " ";
+    }
 
 }
 
