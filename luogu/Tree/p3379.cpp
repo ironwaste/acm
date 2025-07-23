6f11b99d5904625db013f29b76d7d630942e1e78
freshsun-y9000p <ironwaste97213@outlook.com>
2025-07-23 23:27:42 +0800

    优化了一下LCA的时间复杂度还是美国

--- luogu/Tree/p3379.cpp
+++ luogu/Tree/p3379.cpp
@@ -30,13 +30,19 @@ i64 num[N], top[N],is_tp[N],height[N],f[N];
 vector<vector<i64> >g;
 void dfs(i64 fa, i64 u, i64 tp,i64 step) {
     f[u] = fa;
-    top[u] = tp;
     height[u] = step;
+    top[u] = tp;
     i64 next_top = tp;
     if (is_tp[u]) { next_top = u; }
+    int cnt = 0;
     for (auto v : g[u]) {
         if (v == fa)continue;
-        dfs(u, v, next_top,step+1LL);
+        if (!cnt) {
+            cnt++;
+            dfs(u, v, next_top, step + 1LL);
+        } else {
+            dfs(u, v, v, step + 1LL);
+        }
     }
 }
 
@@ -60,25 +66,39 @@ void solve() {
     // for (int i = 1;i <= n;i ++) {
     //     de(i)de(height[i])deb(top[i])
     // }
-    
+    //如何快速判断两者在同一条链上
     while (m--) {
         i64 a, b;
         cin >> a >> b;
-        while (top[a] != top[b]) {
-            while (top[a] != top[b] && height[a] >= height[b]) {a = top[a];}
-            while (top[a] != top[b] && height[a] <= height[b]) {b = top[b];}
+        while (top[a] != top[b] && a != top[b] && b != top[a]) {
+            if(height[a] >= height[b]) {
+                if (a == top[a]) {
+                    a = f[a];
+                } else {
+                    a = top[a];
+                }
+            }else {
+                if (b == top[b]) {
+                    b = f[b];
+                } else {
+                    b = top[b];
+                }
+
+                // b = top[b];
+            }
         }
         
-        if (height[a] < height[b]) { swap(a, b); }
-        i64 tpa = top[a];
-        while (a != b && a!= tpa) {
-            a = f[a];
-        }
+        if (height[a] > height[b]) { swap(a, b); }
+        // i64 tpa = top[a];
         cout << a << endl;
     }
 }
 
 // 2025.07.23——23:07:28 TLE了嗡嗡嗡
+// 2025.07.23——23:25:07 TLE 但是相较于之前少TLE了一个，现在TLE三个
+// 修改部分，将一开始无法判断 是否在同一条链上，修改了一下，将第一次到达的子节点的top值
+// 设置为正常的分支之前的节点，但是对于而后遍历到的节点的top值，修改为了本身
+// 然后去判断是否在同一条链上，在根据其高度得出解
 
 int main(){
     ios_base::sync_with_stdio(false);
