57ddc6675221603aa5f4b5dd997af8ef9960ced0
freshsun-y9000p <ironwaste97213@outlook.com>
2025-08-08 03:21:45 +0800

    杭电第二场 树链剖分终于补完了，但是还是得梳理一下经常会写错

--- hduoj/uni_sch/7-25/1009.cpp
+++ hduoj/uni_sch/7-25/1009.cpp
@@ -24,6 +24,8 @@ using namespace std;
 // 2025.08.07——21:33:19
 // 2025.08.07——22:32:36 中止 写完了 但是在查询最大值的过程中有问题
 // 可能是修改部分
+// 2025.08.08——01:13:56 
+// 2025.08.08——02:45:58 wa 了 第一次觉得wa了那么亲切
 /*
  *
  *
@@ -31,123 +33,113 @@ using namespace std;
 */
 // 对于 dep siz 等多组样例 初始化的问题没想
 
-const int N = 1e5 + 10;
-const int n4 = 4 * N;
-int n, m;
+const int N = 1e5 + 100;
+const int n4 = 4LL * N + 10;
+int n, m, cnt;
 
 i64 dep[N], siz[N], fa[N], dfn[N], id[N];
-i64 top[N],hson[N],lazy[N],seg[n4];
-i64 tree[N],tag[N];
+i64 top[N], hson[N], seg[n4];
+i64 tree[N], tag[N];
 vector<vector<i64> >g;
-i64 dfs1(i64 f, i64 u) {
-    dep[u] = 0;
-    siz[u] = 1LL;
+void init() {
+    for (int i = 0;i < N;i ++) {
+        dep[i] = siz[i] = fa[i] = dfn[i] = id[i] = top[i] = hson[i] = tree[i] = tag[i] = 0;
+    }
+    for (int i = 0;i < n4;i ++) {
+        seg[i] = 0;
+    }
+}
+static inline void dfs1(i64 f, i64 u) {
     dep[u] = dep[f] + 1;
+    siz[u] = 1;
+
+    fa[u] = f;
+    hson[u] = 0;
     for (auto v : g[u]) {
         if (v == f) continue;
+
         fa[v] = u;
-        siz[u] += dfs1(u, v);
+        dfs1(u, v);
+
+        siz[u] += siz[v];
         if (siz[hson[u]] < siz[v]) {
             hson[u] = v;
         }
     }
-    return siz[u];
 }
-int cnt = 1;
-void dfs2(i64 u, i64 tp) {
+static inline void dfs2(i64 u, i64 tp) {
     top[u] = tp;
     id[cnt] = u;
     dfn[u] = cnt++;
     if (hson[u]) {
         dfs2(hson[u], tp);
-        for (auto v : g[u]) {
-            if (v == fa[u] || v == hson[u]) continue;
-            dfs2(v, v);
-        }
+    }
+    for (auto v : g[u]) {
+        if (v == fa[u] || v == hson[u]) continue;
+        dfs2(v, v);
     }
 }
 
-void build(int p,int s,int t) {
-    lazy[p] = 0;
+void build(int p, int s, int t) {
     if (s == t) {
-        seg[p] = tree[ dfn[s] ]; // 时间序 转换为原始序列号
-        tag[s] = 0;
+        seg[p] = tree[id[s]]; // 时间序 转换为原始序列号
         return;
     }
-    int mid = s + (t - s >> 1);
-    int lp = p << 1LL, rp = (p << 1LL) + 1LL;
-    build(lp, s, mid), build(rp, mid + 1, t);
-    seg[p] = max(seg[lp] , seg[rp]);
-}
-void pushdown(int s,int mid,int t,int p) {
-    if (lazy[p]) {
-        int lp = p << 1LL, rp = (p << 1LL) + 1LL;
-        // left son
-        seg[lp] += lazy[p];
-        lazy[lp] += lazy[p];
+    int mid = (s + t) >> 1;
 
-        seg[rp] += lazy[p];
-        lazy[rp] += lazy[p];
-
-        lazy[p] = 0;
-    }
+    build(p * 2, s, mid);
+    build(p * 2 + 1LL, mid + 1, t);
+    seg[p] = max(seg[p * 2], seg[p * 2 + 1LL]);
+    return;
 }
-void add(int p, int s, int t, int l, int r, int x) {
-    if (l <= s && t <= r) {
+
+
+void add(int p, int s, int t, int l, int x) {
+    if (s == t) {
         seg[p] += x;
-        lazy[p] += x;
         return;
     }
 
-    int mid = s + (t - s >> 1);
-    int lp = p << 1LL, rp = (p << 1LL) + 1LL;
+    int mid = (s + t) >> 1;
 
-    pushdown(s, mid, t, p);
-    if (l <= mid) { add(lp, s, mid, l, r, x); }
-    if (r > mid) { add(rp, mid + 1, t, l, r, x); }
 
-    seg[p] = max(seg[lp] , seg[rp]);
+    if (l <= mid) { add(p * 2, s, mid, l, x); } else { add(p * 2 + 1, mid + 1, t, l, x); }
+    seg[p] = max(seg[p * 2], seg[p * 2 + 1]);
+    return;
 }
-
-i64 qry(int p,int s,int t,int l,int r) {
+static inline i64 qry(int p, int s, int t, int l, int r) {
     if (l <= s && t <= r) {
         return seg[p];
     }
-    int mid = s + (t - s >> 1);
-    int lp = p << 1LL, rp = (p << 1LL) + 1LL;
-
-    pushdown(s, mid, t, p);
+    int mid = (s + t) >> 1;
 
     i64 res = 0;
-    if (l <= mid) res = max(res, qry(lp, s, mid, l, r));
-    if (r > mid) res = max(res, qry(rp, mid + 1,t, l, r));
+    if (l <= mid) res = max(res, qry(p * 2, s, mid, l, r));
+    if (r > mid) res = max(res, qry(p * 2 + 1, mid + 1, t, l, r));
     return res;
 }
 
 i64 lcaqry(i64 x, i64 y) {
     i64 res = 0;
     while (top[x] != top[y]) {
-        de(x)de(y)deb(res)
+        if (dep[top[x]] > dep[top[y]]) { swap(x, y); }
+        // i64 fx = top[x];
 
-        if (dep[top[x]] < dep[top[y]]) { swap(x, y); }
-        i64 fx = top[x];
-        // de(fx)de(x)de(y)deb(res)
-        // de(dfn[fx])de(dfn[x])deb(res)
-        res = max(res, qry(1, 1, n, dfn[fx], dfn[x]));
-        x = fx;
-        fx = fa[x];
-        de(dfn[fx])de(dfn[x])deb(res)
-        res = max(res, qry(1, 1, n, dfn[x], dfn[x]) + tag[fx]);
-        x = fx;
+        res = max(res, qry(1, 1, n, dfn[top[y]], dfn[y]));
+        res = max(res, tree[top[y]] + tag[fa[ top[y] ]]);
+        y = fa[top[y]];
     }
-    if (dep[x]< dep[y]) { swap(x, y); }
+    if (dep[x] > dep[y]) { swap(x, y); }
     res = max(res, qry(1, 1, n, dfn[x], dfn[y]));
+    // return res;
+    if (top[x] == x)
+        res = max(res, tree[x] + tag[fa[x]]);
     return res;
 }
 
-void solve() {
+static inline void solve() {
     cin >> n >> m;
-    g.resize(n + 1);
+    g.assign(n + 1, {});
     for (int i = 1;i <= n;i++) {
         cin >> tree[i];
     }
@@ -159,34 +151,42 @@ void solve() {
         g[v].push_back(u);
     }
     cnt = 1;
-    dfs1(1, 1);
+    dfs1(0, 1);
     dfs2(1, 1);
+    // deb(cnt);
     build(1, 1, n);
-    while (m--) {
-        int opt,x,y,z;
+    for (int i = 0;i < m;i++) {
+        int opt, x = 0, y = 0, z = 0;
         cin >> opt;
         if (opt == 1) {
             cin >> x >> y;
-            i64 ans = lcaqry(x, y);
-            deb(ans);
-            // cout <<  << endl;
+            i64 res = lcaqry(x, y);
+            cout << res << endl;
         } else if (opt == 2) {
             cin >> x >> z;
             tag[x] += z;
-            if (x != 1)     add(1, 1, n, dfn[fa[x]], dfn[fa[x]], z);
-            if (hson[x])    add(1, 1, n, dfn[hson[x]], dfn[hson[x]], z);
-            
+            if (fa[x]) {
+                add(1, 1, n, dfn[fa[x]], z);
+                tree[fa[x]] += z;
+            }
+            if (hson[x])    add(1, 1, n, dfn[hson[x]], z);
+
         }
     }
+    for (auto &u : g) {
+        u.clear();
+    }
+    return;
 }
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     int T = 1;
     cin >> T;
-    while(T--){
+    while (T--) {
+        init();
         solve();
     }
     return 0;
