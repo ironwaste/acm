fcf066bd462e7d8921415dfcb41fd493e6e4e6fc
freshsun-y9000p <ironwaste97213@outlook.com>
2025-10-24 15:42:29 +0800

    补题

--- codeforce/div2/div2_1060/C_2_No_Cost_Too_Great_Hard_Version.cpp
+++ codeforce/div2/div2_1060/C_2_No_Cost_Too_Great_Hard_Version.cpp
@@ -30,10 +30,15 @@ using pll = pair<i64, i64>;
  *
  * 求所有的因子 ， sqrt(n);
  * 那么就是 500 左右 刚好 1e8
+ * 特殊样例 ：
+ * 1
+ * 5 77
+ * 2 19
  *
 */
 const i64 N = 2e5 + 10;
 i64 minp[N];
+vector<set< i64 > >pfac(N + 1);
 void init() {
     minp[1] = 1;
     for (int i = 2;i < N;i++) {
@@ -41,12 +46,76 @@ void init() {
             if (!minp[j])minp[j] = i;
         }
     }
+    for (i64 i = 2;i < N;i++) {
+        i64 x = i;
+        while (x > 1) {
+            i64 v = minp[x];
+            pfac[i].insert(v);
+            while (v == minp[x]) {
+                x /= v;
+            }
+        }
+    }
 }
 
 
+
 void solve() {
-    
-    
+    i64 n;cin >> n;
+    vector<pll>a(n);
+    for (int i = 0;i < n;i++) {
+        cin >> a[i].se;
+    }
+
+    for (int i = 0;i < n;i++) {
+        cin >> a[i].fi;
+    }
+    sort(all(a));
+    i64 ans = a[0].fi + a[1].fi;
+    map<i64, i64>cnt;
+    for (int i = 0;i < n;i++) {
+        i64 v = a[i].se;
+
+        for (auto x : pfac[v]) {
+            if (cnt.count(x)) { ans = 0; }
+            cnt[x]++;
+        }
+    }
+
+    for (int i = 0;i < n;i++) {
+        i64 w = a[i].fi,v = a[i].se;
+        for (auto x : pfac[v]) {
+            cnt[x]--;
+        }
+        for (auto x : pfac[v + 1]) {
+            if (cnt.count(x) && cnt[x] > 0) {
+                ans = min(ans, w);
+            }
+        }
+        for (auto x : pfac[v]) {
+            cnt[x] ++;
+        }
+    }
+
+
+    set<i64>check;
+    for (int i = 1;i < n;i++) {
+        i64 w = a[i].fi, v = a[i].se;
+        for (auto x : pfac[v]) {
+            check.insert(x);
+        }
+    }
+    i64 w = a[0].fi, v = a[0].se;
+    for (auto x : check) {
+        i64 k = x - (v % x);
+        if (k == x) { x = 0; }
+        ans = min(ans, k * w);
+    }
+    cout << ans << endl;
+
+
+
+
 
 
 
@@ -58,6 +127,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+    init();
     int T = 1;
     cin >> T;
     while (T--) {
